d51064e33d473182de36825308e6179b69445df5
Nestor Martinez <nesturron@gmail.com>
2023-10-12 10:23:31 +0200

    My solution

---
+++ codewars/6kyu/02-persistent-bugger/spec/1_mine_spec.rb
@@ -0,0 +1,19 @@
+require "1_mine"
+
+describe "Persistence" do
+
+  it "Should return 0 when you pass it 4" do
+    expect(persistence(4)).to eq(0)
+  end
+
+  it "Should return 3 when you pass it 39" do
+    expect(persistence(39)).to eq(3)
+  end
+
+  it "Other basic tests" do
+    expect(persistence(25)).to eq(2)
+    expect(persistence(999)).to eq(4)
+    expect(persistence(444)).to eq(3)
+  end
+
+end
