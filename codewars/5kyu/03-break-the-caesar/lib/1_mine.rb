744f8b1007da28413f5e27ac95d00258fbdbc17a
Nestor Martinez <nesturron@gmail.com>
2023-10-19 13:04:21 +0200

    Properly solved Break the Caesar

--- codewars/5kyu/03-break-the-caesar/lib/1_mine.rb
+++ codewars/5kyu/03-break-the-caesar/lib/1_mine.rb
@@ -5,61 +5,37 @@ SEPARATE_ENCRYPTED_WORDS = ["DAM", "Mjqqt", "btwqi", "Gur", "dhvpx", "oebja", "s
 WORDS = ["wtf", "hello", "world", "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"].sort.to_set
 
 def shift_characters(phrase, shift)
-  phrase.downcase.chars.map do |char|
-    if char.ord >= 97 && char.ord <= 122
-      if (char.ord - shift) >= 97
-        (char.ord - shift).chr
-      else
-        (26 + char.ord - shift).chr
-      end
+  phrase.chars.map do |char|
+    # We need to always count in the same direction
+    # Thus, we need to check if the character ordinal will need to go from a'z ordinal, 97 to z's ordinal, 122
+    # This ordinals correspond to the letters' positions in the alphabet which are
+    # 0-a (97), 1-b (98), 2-c (99), 3-d (100), ..., 24-y (121), 25-z (122)
+    if (char.ord - shift) >= 97
+      # We don't need to jump from 0 to 26, so we just directly substract
+      (char.ord - shift).chr
+    else
+      # We do jump from 0 to 26 so we do need to add 26
+      (26 + char.ord - shift).chr
     end
   end
 end
 
-def check_words(dictionary, words, shift)
-  all_words_included = dictionary.include?(shift_characters(words[0], shift).join)
-  return all_words_included unless all_words_included
-  words.each do |word|
-    all_words_correct = false unless dictionary.include?(shift_characters(word, shift).join)
-    return all_words_included unless all_words_included
-    # p shift_characters(word, shift).join
+def check_message(message, shift)
+  message.count do |word|
+    WORDS.include?(shift_characters(word, shift).join)
   end
 end
 
 def break_caesar(message)
-  combination_found = false
-  clean_message = message.downcase.split(/[^a-zA-Z]+/)
-  shift = 0
-  until combination_found || shift == 25
-    combination_found = check_words(WORDS, clean_message, shift)
-    shift += 1
+  # Let's sanitize the message, removing anything but letters and separating all words in an array
+  clean_message = message.downcase.split(/[^a-z]+/)
+
+  # Let's create an array which will contain the count of words matching the number of characters we shift
+  shifts = []
+  26.times do |shift|
+    shifts << check_message(clean_message, shift)
   end
-  shift - 1
+
+  # We return the index (which is the shift) for which we have the maximum number of matching words
+  shifts.index(shifts.max)
 end
-
-# def word_shifts(word)
-#   chars_shifts = []
-#   chars = word.downcase.chars
-#   chars.each_index do |index|
-#     if chars[index].ord < chars[index - 1].ord
-#       chars_shifts << (26 + chars[index].ord - chars[index - 1].ord)
-#     else
-#       chars_shifts << (chars[index].ord - chars[index - 1].ord)
-#     end
-#   end
-#   chars_shifts
-# end
-
-# def shift_number(encrypted_word, decrypted_word)
-#   first_encrypted_char = encrypted_word.chars[0]
-#   first_decrypted_char = decrypted_word.chars[0]
-#   shift = 0
-#   if first_decrypted_char.ord < first_encrypted_char.ord
-#     shift = first_encrypted_char.ord - first_decrypted_char.ord
-#   elsif first_decrypted_char.ord == first_encrypted_char.ord
-#     shift = 0
-#   else
-#     shift = 26 + first_encrypted_char.ord - first_decrypted_char.ord
-#   end
-#   shift
-# end
