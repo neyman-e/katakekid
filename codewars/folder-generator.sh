d4471a130f15151e4badc46a8cde35adee4954c8
Nestor Martinez <nesturron@gmail.com>
2023-10-19 13:03:09 +0200

    Added some comments

--- codewars/folder-generator.sh
+++ codewars/folder-generator.sh
@@ -1,10 +1,14 @@
 #! /bin/zsh
 
 set -e
+
+# Go to the kata level folder which we ask to the user
 cd /home/neyman/code/neyman-e/katare-kid/codewars
 read -p "What is the level of the kata?" level
 folderlevel="${level}kyu"
 cd $folderlevel
+
+# Create the new kata folder
 newfoldernumber=`find -maxdepth 1 -type d | wc -l`
 read -p "What's the name of the kata?" kataname
 declare -l kataname
@@ -16,6 +20,7 @@ else
   foldername="0${newfoldernumber}-${kataname// /-}"
 fi
 mkdir $foldername
+
+# Copy the template folder into the created kata folder and open VS Code
 cp -r /home/neyman/code/neyman-e/katare-kid/codewars/kata-template/* $foldername
-cd $foldername
-code ../..
+code ..
