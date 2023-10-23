8cbf38e86dafe0b51a9599f48855075b17028f53
Nestor Martinez <nesturron@gmail.com>
2023-10-23 12:11:39 +0200

    Refactored and documented

--- codewars/5kyu/04-numbers-power-sum-digits/lib/1_mine.rb
+++ codewars/5kyu/04-numbers-power-sum-digits/lib/1_mine.rb
@@ -1,11 +1,20 @@
 def power_sumDigTerm(n)
+  # Initialize the series array with 0, as we will be always prompted with an index over 0 and the array
+  # starts with the index 0
   series = [0]
+
+  # We gotta check powering numbers from 2 to high number i.e. 100
   100.times do |number|
-    number +=1
+
+    # And check them elevated to powers from 2 to "not too high" as the results will be getting huge quickly
     20.times do |power|
-      power += 1
-      series << (number**power) if (number == (number**power).digits.sum) && (number**power).digits.length > 1
+      result = number**power
+
+      # I add here the condition of the result needing to be 10 or higher, as the notation of #times is preferred
+      series << result if result > 9 && number == result.digits.sum
     end
   end
+
+  # We return the n'th value of the sorted array
   series.sort[n]
 end
