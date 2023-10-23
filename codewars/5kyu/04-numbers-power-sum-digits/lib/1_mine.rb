6fb510384b9221d90ee54c7a1e38094870bff931
Nestor Martinez <nesturron@gmail.com>
2023-10-23 12:11:08 +0200

    Solved it

--- codewars/5kyu/04-numbers-power-sum-digits/lib/1_mine.rb
+++ codewars/5kyu/04-numbers-power-sum-digits/lib/1_mine.rb
@@ -1,3 +1,11 @@
-def method_name(parameter1, parameter2)
-
+def power_sumDigTerm(n)
+  series = [0]
+  100.times do |number|
+    number +=1
+    20.times do |power|
+      power += 1
+      series << (number**power) if (number == (number**power).digits.sum) && (number**power).digits.length > 1
+    end
+  end
+  series.sort[n]
 end
