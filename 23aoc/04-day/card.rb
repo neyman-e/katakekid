32d8cc17041cf98d1e16f1aec31928bdd22028b5
Nestor Martinez <nesturron@gmail.com>
2023-12-04 22:44:42 +0100

    Cleaning and documenting

--- 23aoc/04-day/card.rb
+++ 23aoc/04-day/card.rb
@@ -9,10 +9,12 @@ class Card
     @playing_cards_count = 1
   end
 
+  # Returns the card's score based on the number of matching numbers -hits-, doubling for every hit (Part 1)
   def score
     hits > 0 ? 2 ** (hits - 1) :0
   end
 
+  # Returns the card's number of matching numbers -hits- (both parts)
   def hits
     count = 0
     @playing_numbers.each do |number|
--- 23aoc/04-day/card_list.rb
+++ 23aoc/04-day/card_list.rb
@@ -7,6 +7,7 @@ class CardList
     read_cards_file if File.exist?(@input_file)
   end
 
+  # Returns the sum of all cards' scores (Part 1)
   def total_score
     sum = 0
     @cards.each do |card|
@@ -15,6 +16,7 @@ class CardList
     sum
   end
 
+  # Returns the total amount of cards, once they have been replicated (Part 2)
   def total_cards
     replicate_cards
     sum = 0
@@ -26,17 +28,7 @@ class CardList
 
   private
 
-  def replicate_cards
-    @cards.each do |card|
-      index = card.card_number
-      # p @cards[0].playing_cards_count
-      for i in 0..card.hits - 1
-        # @cards[index + i].playing_cards_count += card.hits * card.playing_cards_count if (index + i <= @cards.length - 1)
-        @cards[index + i].playing_cards_count += card.playing_cards_count if (index + i <= @cards.length - 1)
-      end
-    end
-  end
-
+  # Reads the input file and processes all data, appending every card information into the cards array (both parts)
   def read_cards_file
     input = File.open(@input_file)
     input.each do |line|
@@ -45,6 +37,16 @@ class CardList
       winning_numbers = line_inputs[1].split(' ').map { |number| number.to_i }
       playing_numbers = line_inputs[2].split(' ').map { |number| number.to_i }
       @cards << Card.new(card_number, winning_numbers, playing_numbers)
+    end
+  end
+
+  # Will add all the copies of every single card, because the previous ones have several matching numbers (Part 2)
+  def replicate_cards
+    @cards.each do |card|
+      index = card.card_number
+      for i in 0..card.hits - 1
+        @cards[index + i].playing_cards_count += card.playing_cards_count if (index + i <= @cards.length - 1)
+      end
     end
   end
 end
