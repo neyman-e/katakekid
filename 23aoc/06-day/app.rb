e354c5e6ef3ae58888a2431102d7ba3dcf09248f
Nestor Martinez <nesturron@gmail.com>
2023-12-06 22:53:13 +0100

    Added benchmarking to test time

--- 23aoc/06-day/app.rb
+++ 23aoc/06-day/app.rb
@@ -1,20 +1,25 @@
 require_relative 'data-parser'
 require_relative 'race-math'
+require 'benchmark'
 
-filename1 = "example.txt"
-filename2 = "input.txt"
+time_spent = Benchmark.measure do
+  filename1 = "example.txt"
+  filename2 = "input.txt"
 
-# Let's parse all data from the 2 files and for the 3 cases (example, part1 and part2)
-data1 = DataParser.new(filename1, "Part1")
-example_race_records = data1.clean_race_records
-data1 = DataParser.new(filename2, "Part1")
-part1_race_records = data1.clean_race_records
-data1 = DataParser.new(filename2, "Part2")
-part2_race_records = data1.clean_race_records
+  # Let's parse all data from the 2 files and for the 3 cases (example, part1 and part2)
+  data1 = DataParser.new(filename1, "Part1")
+  example_race_records = data1.clean_race_records
+  data1 = DataParser.new(filename2, "Part1")
+  part1_race_records = data1.clean_race_records
+  data1 = DataParser.new(filename2, "Part2")
+  part2_race_records = data1.clean_race_records
 
-# Initialize our math class
-math = RaceMath.new
+  # Initialize our math class
+  math = RaceMath.new
 
-puts "The result for the example is #{math.calculate_result(example_race_records)}"
-puts "The result for part 1 is #{math.calculate_result(part1_race_records)}"
-puts "The result for part 2 is #{math.calculate_result(part2_race_records)}"
+  puts "The result for the example is #{math.calculate_result(example_race_records)}"
+  puts "The result for part 1 is #{math.calculate_result(part1_race_records)}"
+  puts "The result for part 2 is #{math.calculate_result(part2_race_records)}"
+end
+
+puts "Execution time: #{time_spent.real * 1000} miliseconds"
