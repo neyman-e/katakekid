8d9e08e2602d269806a198a5e22961cbee61cb88
Nestor Martinez <nesturron@gmail.com>
2023-12-11 21:22:49 +0100

    Part 2 done

--- 23aoc/11-day/app.rb
+++ 23aoc/11-day/app.rb
@@ -5,24 +5,10 @@ require_relative 'data'
 
 # Let's check how much time the whole processing will take the app
 total_time = Benchmark.measure do
-  # Read input data into an array of arrays (having removed the trailing spaces)
+  # Read input data and perform calculations with the Data class
   input_filename = 'input.txt'
   expanded_space_map = Data.new(input_filename)
-  # expanded_space_map.expanded_space_universe.each{ |line| puts line.join}
-
-  # Create our map
-  # map = Map.new(input_data.data)
-
-  # # Output for part 1
-  # puts "The number of steps needed to get to the furthest point is #{map.moves / 2}"
-
-  # # Let's generate an array again to perform the last calculations
-  # map_array = map.to_array
-  # substitutions(map_array)
-  # total_dots = count_dots(map_array)
-
-  # # Output for part 2
-  # puts "There are #{total_dots} tiles enclosed by the loop"
+  puts "The sum of all distances between all galaxies is #{expanded_space_map.distances_between_galaxies.sum}"
 end
 
 # Total time spent in the app
--- 23aoc/11-day/data.rb
+++ 23aoc/11-day/data.rb
@@ -1,15 +1,12 @@
 class Data
-  attr_reader :expanded_space_universe, :distances_between_galaxies
+  attr_reader :distances_between_galaxies
 
   def initialize(filename)
     @filename = filename
-    @expanded_space_universe = []
     read_data if File.exist?(@filename)
-    @space_width = @expanded_space_universe[0].length
-    @space_height = @expanded_space_universe.length
-    @galaxies_list = []
+    @space_height = @vertical_expanded_indexes.length
+    @space_width = @horizontal_expanded_indexes.length
     find_all_galaxies
-    @distances_between_galaxies = []
     measure_distance_between_galaxies
   end
 
@@ -17,60 +14,51 @@ class Data
 
   def read_data
     data_input = File.open(@filename)
-    @expanded_space_universe = expand_space(data_input)
+    @rows = data_input.each_line.map(&:chomp).map(&:chars)
+    expand_space
   end
 
-  def expand_space(data)
-    rows = data.each_line.map(&:chomp).map(&:chars)
-    rows = expand_space_by_rows(rows)
-    cols = expand_space_by_rows(rows.transpose)
-    cols.transpose
+  def expand_space
+    @vertical_expanded_indexes = expand_indexes(@rows)
+    @horizontal_expanded_indexes = expand_indexes(@rows.transpose)
   end
 
-  def expand_space_by_rows(lines)
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-    #                                       #
-    # FALTA ACABAR D'INTENTAR NETEJAR CODI  #
-    # INTENTANT ACABAR D'IMPLEMENTAR        #
-    # LA PART COMENTADA I ESBORRANT L'ALTRA #
-    #                                       #
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-    # lines.reverse.each_with_index do |row, index|
-    #   lines.insert(lines.length - index - 1, Array.new(row.length, '.')) unless row.include?('#')
-    # end
-    # lines
-
-    empty_lines_indexes = []
-    lines.each_with_index do |row, index|
-      empty_lines_indexes << index unless row.include?('#')
+  def expand_indexes(lines)
+    expansion = 1_000_000
+    new_indexes = []
+    lines.each do |row|
+      previous = new_indexes.empty? ? -1 : new_indexes.last
+      if row.include?('#')
+        new_indexes << previous + 1
+      else
+        new_indexes << previous + expansion
+      end
     end
-    row_length = lines[0].length
-    empty_lines_indexes.reverse.each do |index|
-      lines.insert(index, Array.new(row_length, '.'))
-    end
-    lines
+    new_indexes
   end
 
   def find_all_galaxies
+    @galaxies_list = []
     @space_height.times do |vert_coord|
       @space_width.times do |horiz_coord|
-        @galaxies_list << [vert_coord, horiz_coord] if @expanded_space_universe[vert_coord][horiz_coord] == '#'
+        if @rows[vert_coord][horiz_coord] == '#'
+          @galaxies_list << [@vertical_expanded_indexes[vert_coord], @horizontal_expanded_indexes[horiz_coord]]
+        end
       end
     end
   end
 
   def measure_distance_between_galaxies
+    @distances_between_galaxies = []
     pending_galaxies = @galaxies_list.dup
-    @galaxies_list.each_with_index do |orig_coords, index|
+    @galaxies_list.each do |orig_coords|
       pending_galaxies.shift
-      pending_galaxies.each_with_index do |dest_coords, j_index|
+      pending_galaxies.each do |dest_coords|
         vertical_distance = [dest_coords[0], orig_coords[0]].max - [dest_coords[0], orig_coords[0]].min
         horizontal_distance = [dest_coords[1], orig_coords[1]].max - [dest_coords[1], orig_coords[1]].min
         distance = vertical_distance + horizontal_distance
-        @distances_between_galaxies << distance.abs
+        @distances_between_galaxies << distance
       end
     end
-    puts "The sum is #{@distances_between_galaxies.sum}"
   end
 end
