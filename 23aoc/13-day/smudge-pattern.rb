9c107778527064a67c1d0438448c4fed4b728c72
Nestor Martinez <nesturron@gmail.com>
2023-12-14 00:30:07 +0100

    Cleaned but not documented

--- 23aoc/13-day/smudge-pattern.rb
+++ 23aoc/13-day/smudge-pattern.rb
@@ -1,26 +1,17 @@
 class SmudgePattern
   attr_reader :result
 
-  def initialize(pattern, pattern_number)
-    puts "         ---------         Pattern - #{pattern_number}         ---------         "
+  def initialize(pattern)
     rows_array = pattern.map(&:chars)
     @rows = rows_array.map(&:join)
     @cols = rows_array.transpose.map(&:join)
-    puts
-    puts "Checking rows"
     @result = find_reflection(@rows) * 100
-    puts
-    puts "Checking columns"
     @result = find_reflection(@cols) if @result == 0
-    puts
-    puts "Result: #{@result}"
-    puts
   end
 
   private
 
   def find_reflection(lines)
-    lines.each_with_index {|line, index| printf("%02d: %s\n", index, line)}
     for i in 0...lines.length
       for j in i...lines.length
         if one_difference?(lines[i], lines[j])
@@ -34,10 +25,6 @@ class SmudgePattern
   end
 
   def fix_smudge(lines, i, j)
-    puts
-    printf "%02d - %s\n", i, lines[i]
-    printf "%02d - %s\n", j, lines[j]
-    puts
     pattern_lines = lines.dup
     string1 = lines[i].chars
     string2 = lines[j].chars
@@ -62,8 +49,6 @@ class SmudgePattern
       (0 - lines_to_remove).times { |i| first_part.shift }
       first_part.reverse!
     end
-    # p first_part
-    # p second_part
     first_part == second_part
   end
 
